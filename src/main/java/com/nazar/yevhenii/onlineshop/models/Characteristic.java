09eec31545468f6a73b4ab4095cf1092bb181a1a
Nazar <tjnazar1999@gmail.com>
2023-07-03 15:27:37 +0300

    Characteristic model

---
+++ src/main/java/com/nazar/yevhenii/onlineshop/models/Characteristic.java
@@ -0,0 +1,31 @@
+package com.nazar.yevhenii.onlineshop.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Characteristic {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String value;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Type type;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Product> products = new ArrayList<>();
+}
